aa18e5c74eab5c3520139f8eb3e2f5b711933195
sasha <sashaputsik@gmail.com>
2021-01-03 22:41:51 +0300

    starter pack

---
+++ News/News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Sasha Putsikovich on 03.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
